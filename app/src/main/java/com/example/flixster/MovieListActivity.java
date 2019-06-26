ce5f224f84df46e3891df201b3e61d9f555924cf
Hannah Cole <hannahcole@fb.com>
2019-06-26 14:02:54 -0700

    changed activity name

--- app/src/main/java/com/example/flixster/MovieListActivity.java
+++ app/src/main/java/com/example/flixster/MovieListActivity.java
@@ -3,7 +3,7 @@ package com.example.flixster;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MovieListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
